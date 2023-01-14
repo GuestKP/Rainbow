5e34ebd6b373d76fcfb67e700527e56158785aaa
Pavel <kuklinpavel@ngs.ru>
2023-01-14 11:13:55 +0000

    + callbacks and code for AsyncWindow

--- Rainbow.py
+++ Rainbow.py
@@ -1,6 +1,36 @@
-from asyncwindow import AsyncWindow
+from asyncwindow import *
 from time import time, sleep
+import pygame
 
-window = AsyncWindow(500, 500)
+
+def draw_window(surface: pygame.Surface, width: int, height: int) -> None:
+    # example code for this function
+    surface.fill((255, 255, 255))
+
+
+def event_callback(event: pygame.event.Event) -> bool:
+    # example code for this function
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        print('Mouse button', event.button, 'pressed at', event.pos, f'({event})')
+
+    return True  # should return True for execution to continue
+
+
+def keys_checker(keys: list[int]) -> bool:
+    # example code for this function
+    if keys[pygame.K_SPACE] == KEY_PRESSED:
+        print("Space pressed")
+    if keys[pygame.K_SPACE] == KEY_HOLD:
+        pass  # do nothing while space is *held*
+    if keys[pygame.K_SPACE] == KEY_RELEASED:
+        print("Space released")
+
+    return True  # should return True for execution to continue
+
+
+window = AsyncWindow(500, 500, draw_window, event_callback, keys_checker)
+window.run()
 while window.is_running():
-    sleep(1)
+    sleep(1)  # serial read and other things
+
+# here should be serial.close()
--- asyncwindow.py
+++ asyncwindow.py
@@ -1,41 +1,98 @@
-from time import time, sleep, strftime
 from threading import Thread
 import os
+import pygame
+from typing import Callable, Any
+
+KEY_FREE = 0
+KEY_RELEASED = 1
+KEY_PRESSED = 2
+KEY_HOLD = 3
 
 
 class AsyncWindow:
-    window = None
-    width, height = 0, 0
+    surface = None
     last_keys = None
+    event_callback = None
 
-    def is_running(self):
+    def is_running(self) -> bool:
         return self.pygame_thread.isAlive()
 
-    def __init__(self, width, height):
-        self.width, self.height = width, height
-        self.pygame_thread = Thread(target=self.pygame_thread_main)
+    def __init__(self, width: int, height: int, draw_window: Callable[[pygame.Surface, int, int], Any],
+                 event_callback: Callable[[pygame.event.Event], bool], keys_update: Callable[[list[int]], bool],
+                 is_resizable: bool = False) -> None:
+        """
+        Main constructor for AsyncWindow. To run defined window, call AsyncWindow.run().
+        :param width: default width of window in pixels
+        :param height: default height of window in pixels
+        :param draw_window: external function for drawing on window
+        :param event_callback: external function for event handling.
+                    Should return *True* if window must continue execution; *False* otherwise
+        :param keys_update: external function for keys state checking.
+                    Should return *True* if window must continue execution; *False* otherwise
+        :param is_resizable: flag: can user resize window
+        """
+        self.pygame_thread = Thread(target=self.pygame_thread_main,
+                                    args=[width, height, is_resizable])
+        self.event_callback = event_callback
+        self.keys_update = keys_update
+        self.draw_window = draw_window
+
+    def run(self) -> None:
+        """
+        Runs window with options defined by constructor of this class.
+        """
         self.pygame_thread.run()
 
-    def pygame_thread_main(self):
+    def pygame_thread_main(self, width, height, is_resizable) -> None:
+        """
+        Main function for window thread. Executes callbacks, checks event and other things related to window.\n
+        Should **NOT** be called manually.
+        """
         import pygame
+
+        self.init_vars(width, height, is_resizable)
+
+        while True:
+            if self.manage_events():
+                break
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LALT] and keys[pygame.K_F4]:
+                break
+            if not self.keys_update([int(keys[i]) * 2 + int(self.last_keys[i]) for i in range(512)]):
+                break
+            self.last_keys = keys
+
+            self.draw_window(self.surface, self.surface.get_width(), self.surface.get_height())
+            pygame.display.update()
+            pygame.display.flip()
+
+        exit()  # closes thread
+
+    def init_vars(self, width: int, height: int, is_resizable: bool) -> None:
+        """
+        Initializes window and internal vars.\n
+        Should **NOT** be called manually.
+        """
 
         pygame.init()
         os.environ["SDL_VIDEO_CENTERED"] = "1"
-        self.window = pygame.display.set_mode((self.width, self.height))
+        self.surface = pygame.display.set_mode((width, height), pygame.RESIZABLE if is_resizable else 0)
         pygame.display.set_caption("Labyrinth")
         self.last_keys = pygame.key.get_pressed()
-        run = True
 
-        while run:
-            keys = pygame.key.get_pressed()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    break
-            if keys[pygame.K_ESCAPE] or (keys[pygame.K_LALT] and keys[pygame.K_F4]):
-                run = False
-                pass
-            self.last_keys = keys
+    def manage_events(self) -> bool:
+        """
+        Manages events get from pygame window.
+        :return: **True** if window should stop execution and close; **False** otherwise.
+        """
 
-        raise SystemExit()
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            elif event.type == pygame.VIDEORESIZE:
+                self.surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            else:
+                if not self.event_callback(event):
+                    return True
+        return False
